b432fe90405fbd15a49ca62d3a7ad97dfd289e29
farha-begum <farhabegum2708@gmail.com>
2025-05-02 01:06:54 -0400

    Add files via upload

---
+++ common values in array.py
@@ -0,0 +1,5 @@
+array1=[6,7,8,9,10]
+array2=[2,4,6,8,10]
+common_values=set(array1).intersection(set(array2))
+common_values=list(common_values)
+print("common values between the two arrays:",common_values)
---
+++ list,tuple into array.py
@@ -0,0 +1,7 @@
+import array
+my_list=[1,2,3,4,5]
+array_from_list=array.array('i',my_list)
+my_tuple=(2,4,6,8,10)
+array_from_tuple=array.array('i',my_tuple)
+print("array_from_list:",array_from_list)
+print("array_from_tuple:",array_from_tuple)
